76f1714e851e730d1cc45243da6de83e14f0b21e
vyago <josevicente.yago@um.es>
2024-03-03 03:25:32 +0100

    basic structure

--- data-scripts/coinbase_direct.py
+++ data-scripts/coinbase_direct.py
@@ -8,24 +8,26 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 
+
 def on_message(message):
     print(message)
+
 
 def on_error(error):
     print(error)
 
+
 def on_close(close_status_code, close_msg):
     print("### closed ###")
+
 
 def on_open(ws):
     print("Opened connection")
 
+
 if __name__ == "__main__":
     ws_client = WSBase(
-        api_key=API_KEY, 
-        api_secret=API_SECRET, 
-        on_message=on_message, 
-        verbose=True
+        api_key=API_KEY, api_secret=API_SECRET, on_message=on_message, verbose=True
     )
     ws_client.open()
     ws_client.subscribe(["BTC-USD"], ["candles"])
